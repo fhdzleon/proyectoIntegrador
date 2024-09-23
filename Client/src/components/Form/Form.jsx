669de98d613f74b5c6f8ea94fc3914284bdac7fb
fhdzleon <fhdzleon@gmail.com>
2024-09-22 20:45:04 -0600

    for deploy

--- Client/src/components/Form/Form.jsx
+++ Client/src/components/Form/Form.jsx
@@ -10,17 +10,17 @@ const Form = (props) => {
     password: "",
   });
 
-const [errors, setErrors] = useState ({
+  const [errors, setErrors] = useState({
     email: "",
     password: "",
-})  
+  });
 
   const handleChange = (event) => {
     const property = event.target.name;
     const value = event.target.value;
 
     setUserData((prevUserData) => ({ ...prevUserData, [property]: value }));
-}
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -28,48 +28,55 @@ const [errors, setErrors] = useState ({
     const validationErrors = validation(userData);
     setErrors(validationErrors);
 
-    if(Object.keys(validationErrors).length === 0) {
-        props.login(userData);
+    if (Object.keys(validationErrors).length === 0) {
+      props.login(userData);
     }
-  }
+  };
 
   return (
     <>
-    <div className={style.welcome} >
+      <div className={style.welcome}>
         <h1>Rick and Morty App</h1>
         <img src={rick} alt="rick" />
         <img src={morty} alt="morty" />
         <h2>Identificate:</h2>
-    </div>
+      </div>
 
-    <div className={style.form}>
+      <div className={style.form}>
+        <form onSubmit={handleSubmit}>
+          <div className={style.label}>
+            <label htmlFor="email">e-mail:</label>
+            <input
+              type="text"
+              name="email"
+              value={userData.email}
+              onChange={handleChange}
+              placeholder="henry@mail.com"
+            />
+            <span>usar: henry@mail.com</span>
+            {errors.email && (
+              <span className={style.error}>{errors.email}</span>
+            )}
+          </div>
 
-      <form onSubmit={handleSubmit} >
-        <div className={style.label}>
-          <label htmlFor="email">e-mail:</label>
-          <input 
-          type="text" 
-          name="email" 
-          value={userData.email} 
-          onChange={handleChange}
-          />
-          {errors.email && <span className={style.error}>{errors.email}</span>}
-        </div>
+          <div className={style.label}>
+            <label htmlFor="password">password:</label>
+            <input
+              type="text"
+              name="password"
+              value={userData.password}
+              onChange={handleChange}
+              placeholder="pepito123"
+            />
+            <span>usar: pepito123</span>
+            {errors.password && (
+              <span className={style.error}>{errors.password}</span>
+            )}
+          </div>
 
-        <div className={style.label}>
-          <label htmlFor="password">password:</label>
-          <input 
-          type="text" 
-          name="password" 
-          value={userData.password} 
-          onChange={handleChange}
-          /> 
-          {errors.password && <span className={style.error}>{errors.password}</span>}
-        </div>
-
-        <input type="submit" value="Ingresar" className={style.submit} />
-      </form>
-    </div>
+          <input type="submit" value="Ingresar" className={style.submit} />
+        </form>
+      </div>
     </>
   );
 };
--- Client/src/views/Detail.jsx
+++ Client/src/views/Detail.jsx
@@ -8,15 +8,15 @@ const Detail = () => {
   const [character, setCharacter] = useState({});
 
   useEffect(() => {
-   // axios(`https://rickandmortyapi.com/api/character/${id}`).then( 
-    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(  
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      /*  axios(`http://localhost:3001/rickandmorty/character/${id}`).then( */
       ({ data }) => {
         if (data.name) {
           setCharacter(data);
         } else {
           window.alert("No hay personajes con ese ID");
         }
-        console.log(character)
+        console.log(character);
       }
     );
     return setCharacter({});
@@ -26,19 +26,37 @@ const Detail = () => {
     <div className={style.contenedor}>
       <div>
         <div className={style.contenedor2}>
-        <div className={style.detalle}>
-          <h1 className={style.nombreDetalle}>{character.name}</h1>
-          <div className={style.linea}><h2 className={style.args}>STATUS  |  </h2><h1 className={style.info}>{character.status}</h1></div>
-          <div className={style.linea}><h2 className={style.args}>ESPECIE  |  </h2><h1 className={style.info}>{character.species}</h1></div>
-          <div className={style.linea}><h2 className={style.args}>GENERO  |  </h2><h1 className={style.info}>{character.gender}</h1></div>
-         {/* <div className={style.linea}><h2 className={style.args}>ORIGEN  |  </h2><h1 className={style.info}>{character.origin?.name }</h1></div>  API EXTERNO USAR ESTA LINEA Y COMENTAR LA DE ABAJO*/}
-          <div className={style.linea}><h2 className={style.args}>ORIGEN  |  </h2><h1 className={style.info}>{character.origin }</h1></div> 
-        <Link to="/home"><button className={style.boton}>Ir a home </button></Link>
-      
-        <Link to="/favorites"><button className={style.boton}> Ir a favs</button></Link>
+          <div className={style.detalle}>
+            <h1 className={style.nombreDetalle}>{character.name}</h1>
+            <div className={style.linea}>
+              <h2 className={style.args}>STATUS | </h2>
+              <h1 className={style.info}>{character.status}</h1>
+            </div>
+            <div className={style.linea}>
+              <h2 className={style.args}>ESPECIE | </h2>
+              <h1 className={style.info}>{character.species}</h1>
+            </div>
+            <div className={style.linea}>
+              <h2 className={style.args}>GENERO | </h2>
+              <h1 className={style.info}>{character.gender}</h1>
+            </div>
+            <div className={style.linea}>
+              <h2 className={style.args}>ORIGEN | </h2>
+              <h1 className={style.info}>{character.origin?.name}</h1>
+            </div>
+            {/*    <div className={style.linea}>
+              <h2 className={style.args}>ORIGEN | </h2>
+              <h1 className={style.info}>{character.origin}</h1>
+            </div> */}
+            <Link to="/home">
+              <button className={style.boton}>Ir a home </button>
+            </Link>
 
-        </div>
-          <img className={style.image} src={character.image} alt="idimage"/>
+            <Link to="/favorites">
+              <button className={style.boton}> Ir a favs</button>
+            </Link>
+          </div>
+          <img className={style.image} src={character.image} alt="idimage" />
         </div>
       </div>
     </div>
